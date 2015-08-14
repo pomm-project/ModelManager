6fe6323f1af093919f433a91efcb0e93daceb2dd
Grégoire HUBERT <hubert.greg@gmail.com>
2015-08-14 20:40:02 +0200

    Protect FlexibleEntityTrait status
    Entity classes that use the FlexibleEntityTrait were not protected
    against non integer status sent through the `status($status)` method.
    
    Close #46

--- sources/lib/Model/FlexibleEntity/StatefulEntityTrait.php
+++ sources/lib/Model/FlexibleEntity/StatefulEntityTrait.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the PommProject/ModelManager package.
  *
- * (c) 2014 Grégoire HUBERT <hubert.greg@gmail.com>
+ * (c) 2014 - 2015 Grégoire HUBERT <hubert.greg@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,11 +15,11 @@ namespace PommProject\ModelManager\Model\FlexibleEntity;
  * Entities with the ability to keep record of their modification or
  * persistence status.
  *
- * @package ModelManager
- * @copyright 2014-2015 Grégoire HUBERT
- * @author Grégoire HUBERT
- * @license X11 {@link http://opensource.org/licenses/mit-license.php}
- * @see FlexibleEntityInterface
+ * @package     ModelManager
+ * @copyright   2014-2015 Grégoire HUBERT
+ * @author      Grégoire HUBERT
+ * @license     X11 {@link http://opensource.org/licenses/mit-license.php}
+ * @see         FlexibleEntityInterface
  */
 trait StatefulEntityTrait
 {
@@ -31,7 +31,7 @@ trait StatefulEntityTrait
     public function status($status = null)
     {
         if ($status !== null) {
-            $this->status = $status;
+            $this->status = (int) $status;
 
             return $this;
         }
